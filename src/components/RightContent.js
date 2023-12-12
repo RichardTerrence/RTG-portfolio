04c91b14e6fde8a83fa330d3cfebf08adff1b47e
RichardTerrence <richardterrencego@gmail.com>
2023-12-12 19:17:45 +0800

    first commit

--- src/components/RightContent.js
+++ src/components/RightContent.js
@@ -20,38 +20,41 @@ const RightContent = () => {
 
   return (
     <ThemeContext.Provider value={{ theme, setTheme }}>
-      <div className="rightcontainer" id={theme}>
-        <React.Fragment>
-          <SideNav />
-        </React.Fragment>
-        <div className="right-switch">
-          <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
-        </div>
-        <div className="right-info">
-          <h1>Richard Terrence Go</h1>
-          <p>Front-End Developer</p>
-          <a href="R.Go-CV.pdf" download="R.Go-CV.pdf">
-            <Button className="right-button" style={{ border: "none" }}>
-              <i className="fa fa-download"></i> Download Resume
-            </Button>
-          </a>
-        </div>
-        <div
-          className="right-about"
-          id="about"
-          style={{ textAlign: "justify" }}
-        >
-          <h5>About</h5>
-          <p>
-            Highly motivated and enthusiastic with a strong foundation in
-            front-end designing and coding. Eager to contribute my skill and
-            passion for learning to a dynamic and fast-paced team. Possessing a
-            relentless drive to expand my knowledge and proficiency in trending
-            tech stacks, I am committed to deepening my understanding of
-            cutting-edge technologies. With a blend of creativity, adaptability,
-            and problem-solving skills, I am poised to make a positive impact
-            and thrive in the ever-evolving world of web development.
-          </p>
+      <div className="rightcontainer">
+        <div className="rightcontainer1" id={theme}>
+          <React.Fragment>
+            <SideNav />
+          </React.Fragment>
+          <div className="right-switch">
+            <ReactSwitch onChange={toggleTheme} checked={theme === "dark"} />
+          </div>
+          <div className="right-info">
+            <h1>Richard Terrence Go</h1>
+            <p>Front-End Developer</p>
+            <a href="R.Go-CV.pdf" download="R.Go-CV.pdf">
+              <Button className="right-button" style={{ border: "none" }}>
+                <i className="fa fa-download"></i> Download Resume
+              </Button>
+            </a>
+          </div>
+          <div
+            className="right-about"
+            id="about"
+            style={{ textAlign: "justify" }}
+          >
+            <h5>About</h5>
+            <p>
+              Highly motivated and enthusiastic with a strong foundation in
+              front-end designing and coding. Eager to contribute my skill and
+              passion for learning to a dynamic and fast-paced team. Possessing
+              a relentless drive to expand my knowledge and proficiency in
+              trending tech stacks, I am committed to deepening my understanding
+              of cutting-edge technologies. With a blend of creativity,
+              adaptability, and problem-solving skills, I am poised to make a
+              positive impact and thrive in the ever-evolving world of web
+              development.
+            </p>
+          </div>
         </div>
         <div className="rightcontainer2" id={theme}>
           <Skills className="right-skill" />
