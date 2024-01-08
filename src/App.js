dfbb656411282957271399d61109b6be7821adb7
RichardTerrence <richardterrencego@gmail.com>
2024-01-08 16:37:34 +0800

    another02 commit

--- src/App.js
+++ src/App.js
@@ -1,14 +1,15 @@
 import "bootstrap/dist/css/bootstrap.min.css";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import PortfolioContainer from "./components/PortfolioContainer";
-import LeftContent from "./components/LeftContent";
 
 const App = () => {
   return (
     <>
       <BrowserRouter>
         <PortfolioContainer />
-        <Routes></Routes>
+        <Routes>
+          <Route></Route>
+        </Routes>
       </BrowserRouter>
     </>
   );
