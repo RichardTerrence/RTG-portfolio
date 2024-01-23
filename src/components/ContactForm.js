ed611a7f180fbb4144122c73d14ee22f2064365b
RichardTerrence <richardterrencego@gmail.com>
2024-01-23 10:51:26 +0800

    my commit

--- src/components/ContactForm.js
+++ src/components/ContactForm.js
@@ -1,5 +1,3 @@
-import Button from "react-bootstrap/Button";
-import Form from "react-bootstrap/Form";
 import "./ContactForm.css";
 import React, { useState, useRef } from "react";
 import emailjs from "@emailjs/browser";
@@ -19,7 +17,7 @@ const ContactForm = () => {
       .sendForm(
         "service_tc7839m",
         "template_gq4ntc6",
-        Form.current,
+        form.current,
         "1WcWaTJGP4-sXFwol"
       )
       .then(
@@ -34,55 +32,54 @@ const ContactForm = () => {
   };
 
   return (
-    <>
-      <form className="contact-form" id="contact">
-        <div>
-          <h6 className="contact-lets">Let's Connect, Send me a message</h6>
-          <Form className="C-form" ref={form} onSubmit={sendEmail}>
-            <Form.Group className="mb-3" controlId="formBasicUserName">
-              <Form.Label className="C-label">UserName</Form.Label>
-              <Form.Control
-                type="text"
-                name="UserName"
-                value={userName}
-                onChange={(e) => setUserName(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicEmail">
-              <Form.Label className="C-label">Email</Form.Label>
-              <Form.Control
-                type="text"
-                name="email"
-                value={userEmail}
-                onChange={(e) => setUserEmail(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Form.Group className="mb-3" controlId="formBasicMessage">
-              <Form.Label className="C-label">Message</Form.Label>
-              <Form.Control
-                as="textarea"
-                name="message"
-                value={sendMessage}
-                onChange={(e) => setSendMessage(e.target.value)}
-                required
-              />
-            </Form.Group>
-            <Button
-              className="C-button"
-              style={{
-                border: "none",
-              }}
-              type="submit"
-              value="Send"
-            >
-              Send Message
-            </Button>
-          </Form>
+    <div className="contactForm" id="contact">
+      <h6 className="contact-lets">Let's Connect, Send me a message</h6>
+      <form className="C-form" ref={form} onSubmit={sendEmail}>
+        <div className="formGroup mb-3" controlId="formBasicUserName">
+          <div className="formLabel">UserName</div>
+          <input
+            type="text"
+            name="UserName"
+            value={userName}
+            onChange={(e) => setUserName(e.target.value)}
+            required
+          />
         </div>
+        <div className="formGroup mb-3" controlId="formBasicEmail">
+          <div className="formLabel">Email</div>
+          <input
+            type="text"
+            name="email"
+            value={userEmail}
+            onChange={(e) => setUserEmail(e.target.value)}
+            required
+          />
+        </div>
+        <div className="formGroup mb-3" controlId="formBasicMessage">
+          <div className="formLabel">Message</div>
+          <input
+            style={{ width: "100%", height: "80px" }}
+            as="textarea"
+            name="message"
+            value={sendMessage}
+            onChange={(e) => setSendMessage(e.target.value)}
+            required
+          />
+        </div>
+        <button
+          className="contactButton"
+          style={{
+            border: "none",
+            padding: "5px",
+            borderRadius: "10px",
+          }}
+          type="submit"
+          value="Send"
+        >
+          Send Message
+        </button>
       </form>
-    </>
+    </div>
   );
 };
 
